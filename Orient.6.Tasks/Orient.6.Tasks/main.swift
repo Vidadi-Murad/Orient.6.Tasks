3c68b028aa7f2013aa656aaa84b1b9cd2fd7877d
VidadiMurad <vidad0murad@gmail.com>
2026-05-16 23:51:12 +0400

    Task 5 Finish

--- Orient.6.Tasks/Orient.6.Tasks/main.swift
+++ Orient.6.Tasks/Orient.6.Tasks/main.swift
@@ -24,14 +24,52 @@ print("The Answer: \(sumOfNum)")
 
 //                                   Task 3
 /*
+ //-----------------------------------------
+ 
+ for i in 1...50{
+ if i % 2 == 0 {
+ print("\(i)")
+ }else{}
+ }
+ */
+ //-----------------------------------------
+ 
+//                                   Task 5
 //-----------------------------------------
 
-for i in 1...50{
-    if i % 2 == 0 {
-        print("\(i)")
-    }else{}
+/*
+while true{
+    print("---------------------")
+    print("Start Program ( 1 )")
+    print("Finish Program ( 0 )")
+    print("---------------------")
+
+    let choice: Int = Int(readLine()!) ?? 0
+    
+    if choice == 1 {
+        var checkBool: Bool = true
+
+        print("Enter Number:")
+        let num = Int(readLine()!) ?? 0
+
+        for i in 2...num {
+            if num % i == 0{
+                checkBool = false
+                break
+            }
+        }
+
+        if checkBool == true {
+            print("\(num) is Prime")
+        }else{
+            print("\(num) is Not Prime")
+        }
+
+    }
+    if choice == 0 {
+        print("Program Finish :) ")
+        break
+    }
 }
- *?
+*/
 //-----------------------------------------
-
- //                                   Task 
